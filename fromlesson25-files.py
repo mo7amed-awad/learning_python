ced011cd97e4f52719f2a5e7455ecf9df91747b6
mohamed awad <ma7amedawad@gmail.com>
2024-11-30 23:35:35 +0200

    reading from file

---
+++ fromlesson25-files.py
@@ -0,0 +1,9 @@
+#workers_file=open("textfile","r")
+#print(workers_file.readable()) # return boolean
+#print(workers_file.read()) 
+#print(workers_file.readline()) #read first line and put cursor on the second line to start read from it and so on
+#print(workers_file.readlines()) #read all lines and put it in list
+#print(workers_file.readlines()[0]) #read specfic line 
+#workers_file.close()
+
+
