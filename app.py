c1aed6165c18b78018da5244e00a95e7eed423c1
mohamed awad <ma7amedawad@gmail.com>
2024-11-30 18:02:58 +0200

    strings

---
+++ app.py
@@ -0,0 +1,15 @@
+character_name="mohamed"
+age="22"
+# can only concatenate str (not "int") to str !age=22
+print("my name is "+character_name+"and my age is "+age)
+
+text="Hello"
+print(text.lower())
+print(text.upper())
+print(text.isupper())
+print(text.islower())
+print(len(text))
+
+print(text.index('l'))
+
+print(text.replace('l',''))
