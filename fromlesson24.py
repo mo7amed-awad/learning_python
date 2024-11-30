fa76b23fc60321d229cbd63fae72a57ff5f91fcb
mohamed awad <ma7amedawad@gmail.com>
2024-11-30 21:56:24 +0200

    for loop

---
+++ fromlesson24.py
@@ -0,0 +1,14 @@
+friends=['esrra','safa','mariem']
+
+for friend in friends:
+    print(friend)
+
+for x in range(10):
+    print(x)
+
+for friend in friends:
+    if friend=="mohamed":
+        print (friend)
+        break
+else:
+    print("mohamed not found")
