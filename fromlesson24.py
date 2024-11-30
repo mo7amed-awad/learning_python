353df965ef15dc02eb6103a711892e53316ad0fe
mohamed awad <ma7amedawad@gmail.com>
2024-11-30 23:14:05 +0200

    sets

--- fromlesson24.py
+++ fromlesson24.py
@@ -12,3 +12,14 @@ for friend in friends:
         break
 else:
     print("mohamed not found")
+
+
+################## sets ######################
+#is list but have uniqe items
+
+#print(dir(friends))   #to know methods may apply to this type
+
+name="mohamed"
+unique_set=set(name)
+print(unique_set)
+
